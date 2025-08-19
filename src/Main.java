14987eb7a0cf821a8bd44bb195309dfee18bde21
IgorKaisarov <Igo444@mail.ru>
2025-08-19 20:27:21 +0600

    Task 1

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,25 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // Task 1
+        System.out.println(" Task 1 ");
+
+        int[] job = new int[5];
+        job[0] = 10_000;
+        job[1] = 5_000;
+        job[2] = 21_000;
+        job[3] = 8_000;
+        job[4] = 14_500;
+        int sum = 0;
+
+        for (int k = 0; k < job.length; k++) {
+            sum += job[k];
         }
+        System.out.println(" Сумма трат за месяц составила " + sum + " рублей. ");
+
+
+
+
     }
 }
